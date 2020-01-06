bf96af19c27437d0923d8e086dc2a61f56585f09
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2020-01-06 14:27:41 -0500

    Bump to 2.4.16

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfdicom_rev',
-      version          =   '2.4.14',
+      version          =   '2.4.16',
       description      =   'Process DICOM trees and create JSON summares for the ReV viewer.',
       long_description =   readme(),
       author           =   'FNNDSC',
